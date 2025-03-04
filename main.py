4328d20c94038e1bb6f5e1d0b70e7b42d4090bd5
Victoria Munteanu <munteanuvictoria1@gmail.com>
2025-03-04 11:27:11 +0100

    add Flask and install stripe

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # https://docs.stripe.com/payments/checkout 
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
